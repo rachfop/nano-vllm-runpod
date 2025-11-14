d12bb086cc8e1cd2e97a09c318a5101458743edc
Patrick <patrick.rachford@runpod.io>
2025-11-14 12:10:20 -0800

    Install flash-attn and suppress torch dtype warning

--- nanovllm/config.py
+++ nanovllm/config.py
@@ -27,6 +27,10 @@ class Config:
         assert self.kvcache_block_size % 256 == 0
         assert 1 <= self.tensor_parallel_size <= 8
         self.hf_config = AutoConfig.from_pretrained(self.model_path)
+        if hasattr(self.hf_config, "torch_dtype") and not hasattr(
+            self.hf_config, "dtype"
+        ):
+            self.hf_config.dtype = self.hf_config.torch_dtype
         self.max_model_len = min(
             self.max_model_len, self.hf_config.max_position_embeddings
         )
